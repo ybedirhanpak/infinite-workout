27597ffe6ffe7499ed039c5d886be39a2b0bc794
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-06-28 10:44:26 +0300

    Workout page initial layout

--- src/app/workout/workout.module.ts
+++ src/app/workout/workout.module.ts
@@ -8,13 +8,26 @@ import { WorkoutPageRoutingModule } from './workout-routing.module';
 
 import { WorkoutPage } from './workout.page';
 
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    WorkoutPageRoutingModule
+    WorkoutPageRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: '#78C000',
+      innerStrokeColor: '#C7E596',
+      animationDuration: 300,
+      animation: false,
+      responsive: true,
+      renderOnClick: false,
+    }),
   ],
-  declarations: [WorkoutPage]
+  declarations: [WorkoutPage],
 })
 export class WorkoutPageModule {}
