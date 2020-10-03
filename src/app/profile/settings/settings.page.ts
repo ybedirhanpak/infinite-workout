30ef79a13012a688181c19c781dea4754bfbefc8
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-10-04 00:37:23 +0300

    settings.page: Add dark mode toggle to settings

--- src/app/profile/settings/settings.page.ts
+++ src/app/profile/settings/settings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { ThemeService } from 'src/app/shared/services/theme.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  // Value of if dark mode is used
+  darkMode = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private platform: Platform, private themeService: ThemeService) {
+    this.platform.ready().then(() => {
+      this.themeService.fetchTheme();
+    });
   }
 
+  ngOnInit() {
+    // Change darkMode state according to themeService
+    this.themeService.darkMode.subscribe((value) => {
+      this.darkMode = value;
+    });
+  }
+
+  /**
+   * Changes app theme between light and dark with toggle button event
+   */
+  toggleDarkMode(event: any) {
+    const isDarkChecked = event.detail.checked;
+    this.themeService.setTheme(isDarkChecked);
+  }
 }
