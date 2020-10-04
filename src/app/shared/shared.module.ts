df57a5b78d519a964618ad43f6fa12a78041f41d
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-10-04 11:08:32 +0300

    shared.module: Declare and export dark mode toggle

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { DarkModeToggleComponent } from './components/dark-mode-toggle/dark-mode-toggle.component';
 import { PageLayoutComponent } from './components/page-layout/page-layout.component';
 
 @NgModule({
-  declarations: [PageLayoutComponent],
+  declarations: [PageLayoutComponent, DarkModeToggleComponent],
   imports: [IonicModule.forRoot()],
-  exports: [PageLayoutComponent],
+  exports: [PageLayoutComponent, DarkModeToggleComponent],
 })
 export class SharedModule {}
