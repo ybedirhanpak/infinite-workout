78601c2608925b996110a4f11445312954e8e8dc
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-10-04 11:16:08 +0300

    app.component: Fetch theme on constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ThemeService } from './shared/services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,11 @@ export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private themeService: ThemeService
   ) {
     this.initializeApp();
+    this.themeService.fetchTheme();
   }
 
   initializeApp() {
