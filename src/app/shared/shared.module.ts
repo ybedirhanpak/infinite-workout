9ce2d0a5b1c43ea03c7858fbf37e5b70f62533b4
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-10-04 14:12:42 +0300

    shared.module: Declare and export rest time picker

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { DarkModeToggleComponent } from './components/dark-mode-toggle/dark-mode-toggle.component';
 import { PageLayoutComponent } from './components/page-layout/page-layout.component';
+import { RestTimePickerComponent } from './components/rest-time-picker/rest-time-picker.component';
 
 @NgModule({
-  declarations: [PageLayoutComponent, DarkModeToggleComponent],
+  declarations: [
+    PageLayoutComponent,
+    DarkModeToggleComponent,
+    RestTimePickerComponent,
+  ],
   imports: [IonicModule.forRoot()],
-  exports: [PageLayoutComponent, DarkModeToggleComponent],
+  exports: [
+    PageLayoutComponent,
+    DarkModeToggleComponent,
+    RestTimePickerComponent,
+  ],
 })
 export class SharedModule {}
