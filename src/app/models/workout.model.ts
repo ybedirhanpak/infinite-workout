f500239a12d57fbcc6843afe3c1627f717a77902
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-03-06 00:27:17 +0300

    Update workout model

--- src/app/models/workout.model.ts
+++ src/app/models/workout.model.ts
@@ -1,24 +1,27 @@
 enum Equipment {
-  bodyWeight,
-  barbell,
-  dumbbell,
-  kettlebell,
+  bodyWeight = "Body Weight",
+  barbell = "Barbell",
+  dumbbell = "Dumbbell",
+  kettlebell = "Kettlebell",
 }
 
 interface Exercise {
   id: number;
   name: string;
-  duration: number; // Milliseconds
-  image_url?: string;
-  embed_url?: string; // Youtube video embed link
+  duration: string;
+  repetition?: string;
+  weight?: string;
+  imageUrl?: string;
+  videoUrl?: string; // Youtube video embed link
 }
 
 export interface Workout {
   id: number;
   name: string;
-  duration: number; // Milliseconds
+  duration: string;
   equipments: Equipment[];
   description?: string;
   category?: string;
+  imageUrl?: string;
   exercises: Exercise[];
 }
