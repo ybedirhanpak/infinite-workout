13f3482ba3939b91618e2ac7ccd7f812baa983bf
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-03-05 22:08:25 +0300

    Create work-card and work-slide components

---
+++ src/app/shared/components/workout-card/workout-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-card',
+  templateUrl: './workout-card.component.html',
+  styleUrls: ['./workout-card.component.scss'],
+})
+export class WorkoutCardComponent implements OnInit {
+
+  @Input() name: string = 'Name';
+  @Input() category: string = 'Category';
+  @Input() imageUrl: string = 'assets/img/light-theme.png';
+  @Input() duration: string = '10 min';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,17 @@ import { IonicModule } from '@ionic/angular';
 import { DarkModeToggleComponent } from './components/dark-mode-toggle/dark-mode-toggle.component';
 import { PageLayoutComponent } from './components/page-layout/page-layout.component';
 import { RestTimePickerComponent } from './components/rest-time-picker/rest-time-picker.component';
+import { WorkoutCardComponent } from './components/workout-card/workout-card.component';
+import { WorkoutSlideComponent } from './components/workout-slide/workout-slide.component';
+
 
 @NgModule({
   declarations: [
     PageLayoutComponent,
     DarkModeToggleComponent,
     RestTimePickerComponent,
+    WorkoutCardComponent,
+    WorkoutSlideComponent
   ],
   imports: [
     CommonModule,
@@ -21,6 +26,8 @@ import { RestTimePickerComponent } from './components/rest-time-picker/rest-time
     PageLayoutComponent,
     DarkModeToggleComponent,
     RestTimePickerComponent,
+    WorkoutCardComponent,
+    WorkoutSlideComponent
   ],
 })
 export class SharedModule {}
