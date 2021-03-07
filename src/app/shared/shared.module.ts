0793c3d301e9f8a45d009738605171db368a64cc
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-03-07 16:07:15 +0300

    Create clock circle component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,8 @@ import { RestTimePickerComponent } from './components/rest-time-picker/rest-time
 import { WorkoutCardComponent } from './components/workout-card/workout-card.component';
 import { WorkoutSlideComponent } from './components/workout-slide/workout-slide.component';
 import { ClickableComponent } from './components/clickable/clickable.component';
+import { ClockCircleComponent } from './components/clock-circle/clock-circle.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 
 
@@ -18,12 +20,31 @@ import { ClickableComponent } from './components/clickable/clickable.component';
     RestTimePickerComponent,
     WorkoutCardComponent,
     WorkoutSlideComponent,
-    ClickableComponent
+    ClickableComponent,
+    ClockCircleComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule.forRoot()
+    IonicModule.forRoot(),
+    NgCircleProgressModule.forRoot({
+      radius: 80,
+      outerStrokeWidth: 12,
+      innerStrokeWidth: 12,
+      space: -12,
+      outerStrokeColor: 'var(--ion-color-primary, black)',
+      innerStrokeColor: 'var(--ion-color-step-200, gray)',
+      responsive: true,
+      renderOnClick: false,
+      animation: false,
+      animateTitle: false,
+      showInnerStroke: true,
+      showSubtitle: false,
+      showUnits: false,
+      titleFontSize: '2.5rem',
+      titleColor: 'var(--ion-text-color, black)',
+      titleFontWeight: 'bold'
+    }),
   ],
   exports: [
     PageLayoutComponent,
@@ -31,7 +52,8 @@ import { ClickableComponent } from './components/clickable/clickable.component';
     RestTimePickerComponent,
     WorkoutCardComponent,
     WorkoutSlideComponent,
-    ClickableComponent
+    ClickableComponent,
+    ClockCircleComponent
   ],
 })
 export class SharedModule {}
