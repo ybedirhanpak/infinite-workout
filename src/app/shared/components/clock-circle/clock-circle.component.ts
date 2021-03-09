0b2616b3b50415f8cddc9ef7207523647653f674
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-03-09 22:07:55 +0300

    Update training page

--- src/app/shared/components/clock-circle/clock-circle.component.ts
+++ src/app/shared/components/clock-circle/clock-circle.component.ts
@@ -10,21 +10,20 @@ import {
 } from '@angular/core';
 import { DateService } from '@services/date.service';
 
-
 @Component({
   selector: 'app-clock-circle',
   templateUrl: './clock-circle.component.html',
   styleUrls: ['./clock-circle.component.scss'],
 })
-export class ClockCircleComponent implements OnInit, OnDestroy, OnChanges {
+export class ClockCircleComponent implements OnInit, OnDestroy {
+  @Input() id = -1;
   @Input() mode: 'timer' | 'stopwatch' = 'stopwatch';
   @Input() max = 60;
   @Input() current = 0;
-  @Input() pause = false;
-  @Input() reset = false;
 
   @Output() onFinish = new EventEmitter<boolean>();
 
+  paused = true;
   currentString = '00:00';
   percent = 0;
   tickInterval: NodeJS.Timeout;
@@ -41,35 +40,6 @@ export class ClockCircleComponent implements OnInit, OnDestroy, OnChanges {
     }
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if ('pause' in changes) {
-      const { currentValue, previousValue } = changes['pause'];
-      this.onPauseChange(currentValue, previousValue);
-    }
-
-    if ('reset' in changes) {
-      const { currentValue, previousValue } = changes['reset'];
-      this.onResetChange(currentValue, previousValue);
-    }
-  }
-
-  onPauseChange(value: boolean, prev: boolean) {
-    if (value && this.tickInterval) {
-      clearInterval(this.tickInterval);
-    }
-
-    if (!value) {
-      this.tickInterval = setInterval(() => {
-        this.tick();
-      }, 1000);
-    }
-  }
-
-  onResetChange(value: boolean, prev: boolean) {
-    if(value)
-      this.resetClock();
-  }
-
   updateCircle() {
     this.currentString = this.dateService.secondsToString(this.current);
 
@@ -82,7 +52,30 @@ export class ClockCircleComponent implements OnInit, OnDestroy, OnChanges {
     }
   }
 
-  resetClock() {
+  public start() {
+    if (!this.tickInterval) {
+      this.paused = false;
+      this.tickInterval = setInterval(() => {
+        this.tick();
+      }, 1000);
+    }
+  }
+
+  public pause() {
+    if (this.tickInterval) {
+      this.paused = true;
+      clearInterval(this.tickInterval);
+      this.tickInterval = null;
+    }
+  }
+
+  public reset() {
+    // If clock was running before reset, reset and continue
+    // If clock was stopped before, only reset the time
+    const runningBeforeReset = !this.paused;
+
+    if (runningBeforeReset) this.pause();
+
     if (this.mode === 'timer') {
       this.current = this.max;
     }
@@ -92,23 +85,28 @@ export class ClockCircleComponent implements OnInit, OnDestroy, OnChanges {
     }
 
     this.updateCircle();
+
+    if (runningBeforeReset) this.start();
+  }
+
+  public stop() {
+    this.reset();
+    this.pause();
+  }
+
+  public updateMax(max: number) {
+    this.pause();
+    this.max = max;
+    this.reset();
+    this.start();
   }
 
   tick() {
-    if (this.reset) {
-      this.resetClock();
-      return;
-    }
-
-    if (this.pause) {
-      return;
-    }
-
     // Timer mode
     if (this.mode === 'timer' && this.current > 0) {
       this.current--;
 
-      if(this.current === 0) {
+      if (this.current === 0) {
         this.onFinish.emit(true);
       }
     }
@@ -116,7 +114,7 @@ export class ClockCircleComponent implements OnInit, OnDestroy, OnChanges {
     // Stopwatch mode
     if (this.mode === 'stopwatch' && this.current < this.max) {
       this.current++;
-      if(this.current === this.max) {
+      if (this.current === this.max) {
         this.onFinish.emit(true);
       }
     }
