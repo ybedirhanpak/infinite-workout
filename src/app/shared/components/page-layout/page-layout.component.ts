71bb2920472ef58b01f3e2b299dc0ec135a55931
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-03-22 23:07:42 +0300

    Add fullscreen support to page layout

--- src/app/shared/components/page-layout/page-layout.component.ts
+++ src/app/shared/components/page-layout/page-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-layout',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-layout.component.scss'],
 })
 export class PageLayoutComponent implements OnInit {
+  @Input() fullScreen: boolean;
 
   constructor() { }
 
