176d61dc334331c1c5a446ef46d4fe1590e573f4
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-03-27 16:20:18 +0300

    workout.service: Add customized workouts

--- src/app/services/workout.service.ts
+++ src/app/services/workout.service.ts
@@ -3,13 +3,15 @@ import { Storage } from '@ionic/storage';
 
 // Model
 import { Workout } from '@models/workout.model';
-import { LocalList } from '../utils/local-list.util';
+
+// Utils
+import { LocalList } from '@utils/local-list.util';
 import { State } from '@utils/state.util';
 
 // Storage Keys
 const FAVORITE_WORKOUTS_KEY = 'FAVORITE_WORKOUTS';
 const CREATED_WORKOUTS_KEY = 'CREATED_WORKOUTS';
-
+const CUSTOMIZED_WORKOUTS_KEY = 'CREATED_WORKOUTS';
 
 @Injectable({
   providedIn: 'root',
@@ -19,9 +21,11 @@ export class WorkoutService {
 
   public favoriteWorkouts: LocalList<Workout[]>;
   public createdWorkouts: LocalList<Workout[]>;
+  public customizedWorkouts: LocalList<Workout[]>;
 
   constructor(storage: Storage) {
     this.favoriteWorkouts = new LocalList(storage, FAVORITE_WORKOUTS_KEY);
     this.createdWorkouts = new LocalList(storage, CREATED_WORKOUTS_KEY);
+    this.customizedWorkouts = new LocalList(storage, CUSTOMIZED_WORKOUTS_KEY);
   }
 }
