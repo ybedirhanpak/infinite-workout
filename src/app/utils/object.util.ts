855472c3ad77252f61195855432dc8deaeee9ec4
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-03-29 19:49:53 +0300

    object.util.ts: Create groupBy function

--- src/app/utils/object.util.ts
+++ src/app/utils/object.util.ts
@@ -4,3 +4,18 @@ export const copyFrom = (from: any, to: any) => {
   });
   return to;
 };
+
+export const groupBy = (list: any[], name: string, group: string) => {
+  const grouped = {};
+  list.map((element) => {
+    if (grouped[element[group]]) {
+      grouped[element[group]][name].push(element);
+    } else {
+      grouped[element[group]] = {
+        [group]: element[group],
+        [name]: [element],
+      };
+    }
+  });
+  return Object.values(grouped);
+};
