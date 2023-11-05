62db23c63bcb0342b145bd8f530d13d93d530324
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2023-11-05 17:41:12 +0300

    Stop using swiper in workout-slide-component

--- src/app/shared/components/workout-slide/workout-slide.component.ts
+++ src/app/shared/components/workout-slide/workout-slide.component.ts
@@ -13,10 +13,20 @@ export class WorkoutSlideComponent {
   swiperModules = [IonicSlides];
 
   @Input() workouts: Workout[] = [];
+  @Input() cardPerView = 2.2;
+  @Input() spaceBetween = 12;
 
   @Output() onClick = new EventEmitter<Workout>();
 
+  cardWidth: string = "100%";
+  cardPadding: string = "0px";
+
   onWorkoutClick(workout: Workout) {
     this.onClick.emit(workout);
+  }
+
+  ngOnInit() {
+    this.cardWidth = `${(window.innerWidth / (this.cardPerView) - this.spaceBetween)}px`;
+    this.cardPadding = `${this.spaceBetween}px`;
   }
 }
